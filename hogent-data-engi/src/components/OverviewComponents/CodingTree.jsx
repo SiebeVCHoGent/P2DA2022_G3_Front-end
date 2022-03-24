43917f6709d2782050a1f4781b877fbce679dcdc
Siebe Van Campenhout <siebe.vancampenhout@student.hogent.be>
2022-03-24 18:15:57 +0100

    Coding tree border done
    + fix voor kleur van body (flex in html, #root, body)

---
+++ hogent-data-engi/src/components/OverviewComponents/CodingTree.jsx
@@ -0,0 +1,5 @@
+export default function CodingTree(){
+    return <div className="coding-tree-container">
+        
+    </div>
+}
--- hogent-data-engi/src/components/OverviewComponents/Overview.jsx
+++ hogent-data-engi/src/components/OverviewComponents/Overview.jsx
@@ -1,3 +1,4 @@
+import CodingTree from "./CodingTree";
 import DefaultInfoCompany from "./DefaultInfoCompany";
 import Duurzaamheidsscore from "./Duurzaamheidsscore";
 import SearchBar from "./SearchBar";
@@ -8,10 +9,16 @@ export default function Overview()
     return <main>
         <div className="inside-main">
             <h1>Dashboard</h1>
-            <SearchBar/>
-            <DefaultInfoCompany/>
-            
-            <Duurzaamheidsscore/>
+            <div className="main-container">
+                <div className="dashboard-info-container">
+                    <SearchBar/>
+                    <DefaultInfoCompany/>
+                    <Duurzaamheidsscore/>
+                </div>
+                <div>
+                    <CodingTree/>
+                </div>
+            </div>
         </div>
     </main>
 }
