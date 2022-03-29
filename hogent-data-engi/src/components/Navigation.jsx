7cdc3f117752d5a23d68fc039021cdd651ff0650
Siebe Van Campenhout <siebe.vancampenhout@student.hogent.be>
2022-03-29 13:18:14 +0200

    nav collapse implemented

--- hogent-data-engi/src/components/Navigation.jsx
+++ hogent-data-engi/src/components/Navigation.jsx
@@ -1,25 +1,39 @@
+import { useCallback, useMemo, useState } from 'react'
+import { HiOutlineMenuAlt2 } from 'react-icons/hi'
+
 export default function Navigation()
 {
-    return <nav>
-        <div className="title">
-            HOGENT
-            <span className="subtitle">Data Engineering Project</span>
+    const [navHidden, setNavHidden] = useState(false)
+
+    return (
+    <nav>
+        <div  class='nav' hidden={navHidden}>
+            <div className="title">
+                HOGENT
+                <HiOutlineMenuAlt2 className='nav-icon' onClick={() => setNavHidden(!navHidden)}/>
+                <span className="subtitle">Data Engineering Project</span>
+            </div>
+                
+            <div className=".nav-body">
+                <div className="nav-sub">
+                <span className="nav-sub-title">MENU</span>
+                    <ul className="nav-sub-list">
+                        <li>Uitleg</li>
+                        <li>Dashboard</li>
+                        <li>Overzicht</li>
+                    </ul>
+                </div>
+                <div className="nav-sub">
+                    <span className="nav-sub-title">APPLICATIONS</span>
+                    <ul className="nav-sub-list">
+                        <li>Duurzaamheid bij kmo's</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div className=".nav-body">
-            <div className="nav-sub">
-            <span className="nav-sub-title">MENU</span>
-                <ul className="nav-sub-list">
-                    <li>Uitleg</li>
-                    <li>Dashboard</li>
-                    <li>Overzicht</li>
-                </ul>
-            </div>
-            <div className="nav-sub">
-                <span className="nav-sub-title">APPLICATIONS</span>
-                <ul className="nav-sub-list">
-                    <li>Duurzaamheid bij kmo's</li>
-                </ul>
-            </div>
+        <div class='nav-collapsed' hidden={!navHidden}>
+            <HiOutlineMenuAlt2 className='nav-icon-collapsed' onClick={() => setNavHidden(!navHidden)}/>
         </div>
     </nav>
+    )
 }
