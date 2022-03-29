fb484ae856288827b88a6ed9691c0df9e5f033af
Siebe Van Campenhout <siebe.vancampenhout@student.hogent.be>
2022-03-29 12:59:55 +0200

    Icons Toegevoegd (react-icons)

--- hogent-data-engi/src/components/OverviewComponents/SearchBar.jsx
+++ hogent-data-engi/src/components/OverviewComponents/SearchBar.jsx
@@ -2,6 +2,8 @@ import { useContext } from 'react';
 import {useForm} from 'react-hook-form'
 import { SearchContext } from "../../contexts/SearchProvider";
 
+import { ImSearch } from 'react-icons/im'
+
 export default function SearchBar() {
     const { register, handleSubmit} = useForm();
     const { searchKMO } = useContext(SearchContext)
@@ -18,7 +20,7 @@ export default function SearchBar() {
                     placeholder="Zoek op bedrijfsnaam of sector..."
                     {...register('query', {required:true})}
                 ></input>
-                <span className="search-glass" onClick={handleSubmit(onSubmit)}>S</span>
+                <span className="search-glass" onClick={handleSubmit(onSubmit)}><ImSearch/></span>
             </span>
         </form>
     </div>
