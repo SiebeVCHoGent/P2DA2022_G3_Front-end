efe7c024c5e2272f1d11cee81579502c31728b84
Siebe Van Campenhout <siebe.vancampenhout@student.hogent.be>
2022-03-29 12:00:45 +0200

    duurzaamheidsscore implemented

--- hogent-data-engi/src/components/OverviewComponents/Duurzaamheidsscore.jsx
+++ hogent-data-engi/src/components/OverviewComponents/Duurzaamheidsscore.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react"
+import { SearchContext } from "../../contexts/SearchProvider"
+
 export default function Duurzaamheidsscore()
 {
+    const {searchresult: sr} = useContext(SearchContext)
+
     return <div>
         <h2>Duurzaamheidsscore</h2>
         <div className="duurzaam-container">
-            <p className="big-letter duurzaam-letter">C</p>
+            <p className="big-letter duurzaam-letter">{sr?.duurzaamheidsscore ? sr.duurzaamheidsscore : '/'}</p>
             <p className="duurzaam-text">Op basis van het totaal aantal opzoektermen van de coding tree bepalen we de maximale totaalscore die een kmo kan krijgen indien deze over 
                 elke opzoekterm rapporteert. Iedere opzoekterm krijgt 1 punt.
             </p>
