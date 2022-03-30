429a4938be7bd848895a7de62c9efdf39dd5c5f9
Guylian Weverbergh <guylian.weverbergh@student.hogent.be>
2022-03-30 17:39:39 +0200

    Added topright symbols

--- hogent-data-engi/src/App.js
+++ hogent-data-engi/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className='body'>
           <Navigation></Navigation>
+          
           <SearchProvider>
             <Overview></Overview>
           </SearchProvider>
--- hogent-data-engi/src/components/OverviewComponents/Overview.jsx
+++ hogent-data-engi/src/components/OverviewComponents/Overview.jsx
@@ -3,10 +3,29 @@ import DefaultInfoCompany from "./DefaultInfoCompany";
 import Duurzaamheidsscore from "./Duurzaamheidsscore";
 import SearchBar from "./SearchBar";
 
+import Language from "../../files/images/language.png"
+import Settings from "../../files/images/settings.svg"
+import Profile from "../../files/images/profile.jpg"
+
+
+
 export default function Overview() {
     return <main>
         <div className="inside-main">
-            <h1>Dashboard</h1>
+            <header>
+                <h1>Dashboard</h1>
+                <section>
+                    <div>
+                        <a href="#"><img src={Language} alt="settings" /></a>
+                    </div>
+                    <div className="settings">
+                        <a href="#"><img src={Settings} alt="settings"/></a>
+                    </div>
+                    <div className="circel-profile">
+                        <img src={Profile} alt="profile" />
+                    </div>
+                </section>
+            </header>
             <div className="main-container">
                 <div className="dashboard-info-container elements">
                     <SearchBar />
