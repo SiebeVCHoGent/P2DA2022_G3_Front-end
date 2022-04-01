8d06fd19deb3f8eb48f450948755b24f4751d136
Siebe Van Campenhout <siebe.vancampenhout@student.hogent.be>
2022-04-01 19:39:58 +0200

    BUG fix icons

--- hogent-data-engi/src/components/OverviewComponents/Overview.jsx
+++ hogent-data-engi/src/components/OverviewComponents/Overview.jsx
@@ -4,7 +4,7 @@ import Duurzaamheidsscore from "./Duurzaamheidsscore";
 import SearchBar from "./SearchBar";
 
 import Language from "../../files/images/language.png"
-import Settings from "../../files/images/settings.svg"
+import {AiFillSetting} from 'react-icons/ai'
 import Profile from "../../files/images/profile.jpg"
 
 
@@ -14,14 +14,14 @@ export default function Overview() {
         <div className="inside-main">
             <header>
                 <h1>Dashboard</h1>
-                <section>
-                    <div>
+                <section className="top-icons">
+                    <div className="flag">
                         <a href="#"><img src={Language} alt="settings" /></a>
                     </div>
                     <div className="settings">
-                        <a href="#"><img src={Settings} alt="settings"/></a>
+                        <a href="#"><AiFillSetting/></a>
                     </div>
-                    <div className="circel-profile">
+                    <div className="cirkel-profile">
                         <img src={Profile} alt="profile" />
                     </div>
                 </section>
