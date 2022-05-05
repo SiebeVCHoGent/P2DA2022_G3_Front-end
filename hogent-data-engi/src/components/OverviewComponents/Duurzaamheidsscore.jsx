53dab7adc45356fdcee35e87dfe21944b65efea2
Siebe Van Campenhout <siebe.vancampenhout@student.hogent.be>
2022-05-05 16:52:21 +0200

    Percentiel plekje gemaakt
    Co-Authored-By: JarnoBeeckmanhogent <79344588+JarnoBeeckmanhogent@users.noreply.github.com>

--- hogent-data-engi/src/components/OverviewComponents/Duurzaamheidsscore.jsx
+++ hogent-data-engi/src/components/OverviewComponents/Duurzaamheidsscore.jsx
@@ -8,7 +8,10 @@ export default function Duurzaamheidsscore()
     return <div>
         <h2>Duurzaamheidsscore</h2>
         <div className="duurzaam-container">
-            <p className="big-letter duurzaam-letter">{sr?.duurzaamheidsscore ? sr.duurzaamheidsscore : '-'}</p>
+            <div className="big-letters">
+                <p className="big-letter duurzaam-letter">{sr?.duurzaamheidsscore ? sr.duurzaamheidsscore : '-'}</p>
+                <p className={`big-letter ${sr?.percentiel ? 'smaller' : ''} duurzaam-letter`}>{sr?.percentiel ? sr.percentiel : '-'}</p>
+            </div>
             <p className="duurzaam-text">Op basis van het totaal aantal opzoektermen van de coding tree bepalen we de maximale totaalscore die een kmo kan krijgen indien deze over 
                 elke opzoekterm rapporteert. Iedere opzoekterm krijgt 1 punt.
             </p>
